4b29d374b67d842ced7d924ef7d47de7bf0e29e6
Breno Sobral <ssobralbreno@gmail.com>
2023-03-13 18:45:20 -0300

    feat: move landingpage

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className={`bg-background grid gap-y-16 overflow-hidden`}>
+      <div className={`relative bg-background`}>
+        <div className="max-w-7xl mx-auto">
+          <div
+            className={`relative z-10 pb-8 bg-background sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32`}>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default App;
