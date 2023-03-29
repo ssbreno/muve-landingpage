9468952ed30f293f53b5b0b41f06d190500792f8
Everton da Rocha Silva <evertondarocha2018@gmail.com>
2023-03-28 21:33:21 -0300

    breno

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,22 +6,28 @@ export function Hero() {
   const { mainContent, menuContent } = config;
   return (
     <main
-      className="flex overflow-x-hidden flex-col bg-vetor-blue bg-cover md:px-6 lg:px-4 
+      className="flex overflow-x-hidden w-full bg-vetor-blue bg-cover md:px-16 lg:px-4 
     md:justify-center items-center lg:h-screen h-screen"
     >
-      <div className="flex flex-col lg:w-1/2 pt-20 container">
+      <div className="flex flex-col lg:w-1/2 justify-center items-center sm:w-[50%] container">
         <Image
           src={Muve}
-          className="md:w-4/5 sm:mt-20 self-center mt-40 w-90"
+          className="md:w-4/5 mb-4 w-90"
           alt="Muve Logo"
           quality={100}
         />
-        <p className="text-gray-800 self-center font-machina mt-12 font-bold lg:text-5xl text-xl ">
+        <p className="text-gray-800  font-machina  font-bold lg:text-5xl text-xl ">
           {mainContent}
         </p>
-        <p className="text-gray-800 self-center mx-auto font-machina font-normal text-base">
+        <p className="text-gray-800  font-machina -mt-[1.2rem] font-normal text-base">
           {menuContent}
         </p>
+      </div>
+      <div className="w-[50%] flex justify-center items-center">
+        <img
+          src="assets/images/app-photos/deliveryman.png"
+          className="w-[40%] blend-multiply"
+        />
       </div>
     </main>
   );
