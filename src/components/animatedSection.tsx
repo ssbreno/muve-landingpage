f087c79b8677817ec3544afb834cdb9c6eeafbb1
Breno Sobral <ssobralbreno@gmail.com>
2023-05-07 11:56:46 -0300

    feat: finish muve delivery

--- src/components/animatedSection.tsx
+++ src/components/animatedSection.tsx
@@ -12,40 +12,40 @@ export function AnimatedSection() {
                   className="inline-flex"
                   src="/assets/images/partners/pagar-me.png"
                   alt=""
-                  width={180}
-                  height={180}
+                  width={220}
+                  height={220}
                   quality={100}
                 />
                 <Image
                   className="inline-flex "
                   src="/assets/images/partners/inter.png"
                   alt=""
-                  width={180}
-                  height={180}
+                  width={220}
+                  height={220}
                   quality={100}
                 />
                 <Image
                   className="inline-flex "
                   src="/assets/images/partners/delivery-direto.png"
                   alt=""
-                  width={180}
-                  height={180}
+                  width={220}
+                  height={220}
                   quality={100}
                 />
                 <Image
                   className="inline-flex"
                   src="/assets/images/partners/neemo.png"
                   alt=""
-                  width={180}
-                  height={180}
+                  width={220}
+                  height={220}
                   quality={100}
                 />
                 <Image
                   className="inline-flex"
                   src="/assets/images/partners/accon.png"
                   alt=""
-                  width={180}
-                  height={180}
+                  width={220}
+                  height={220}
                   quality={100}
                 />
               </div>
@@ -56,32 +56,32 @@ export function AnimatedSection() {
                   className="inline-flex"
                   src="/assets/images/partners/inter.png"
                   alt=""
-                  width={180}
-                  height={180}
+                  width={220}
+                  height={220}
                   quality={100}
                 />
                 <Image
                   className="inline-flex"
                   src="/assets/images/partners/delivery-direto.png"
                   alt=""
-                  width={180}
-                  height={180}
+                  width={220}
+                  height={220}
                   quality={100}
                 />
                 <Image
                   className="inline-flex"
                   src="/assets/images/partners/neemo.png"
                   alt=""
-                  width={180}
-                  height={180}
+                  width={220}
+                  height={220}
                   quality={100}
                 />
                 <Image
                   className="inline-flex"
                   src="/assets/images/partners/accon.png"
                   alt=""
-                  width={180}
-                  height={180}
+                  width={220}
+                  height={220}
                   quality={100}
                 />
               </div>
