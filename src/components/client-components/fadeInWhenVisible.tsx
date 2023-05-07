7ee2ee244c2906d05b9286c1e2709f5db86280da
Breno Sobral <ssobralbreno@gmail.com>
2023-05-07 10:55:24 -0300

    feat: slow animations

--- src/components/client-components/fadeInWhenVisible.tsx
+++ src/components/client-components/fadeInWhenVisible.tsx
@@ -7,7 +7,7 @@ export default function FadeInWhenVisible({ children }: any) {
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true }}
-      transition={{ duration: 0.3 }}
+      transition={{ duration: 1.0 }}
       variants={{
         visible: { opacity: 1, scale: 1 },
         hidden: { opacity: 0, scale: 0 },
